a9c7a3e430ac90e6bddfcce6e9689c4d115bb46d
Amandeep Singh <programmeramandeep@gmail.com>
2022-05-21 16:33:56 +0530

    Implemented top navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,13 @@ import { useStateContext } from "../contexts/ContextProvider";
 import { links } from "../data/dummy";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -21,7 +27,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-light dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -51,7 +57,7 @@ const Sidebar = () => {
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                   >
                     {link.icon}
                     <span className="capitalize">{link.name}</span>
