f022537fd840174b9408ec9311f5f2cfb7b8cf78
Amandeep Singh <programmeramandeep@gmail.com>
2022-05-21 17:33:51 +0530

    Implemented homepage dashboard upper section

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ color, text, bgColor, size, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
